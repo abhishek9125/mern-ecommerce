4c18e35707713ed125c34688eb45c128bf8f945e
agarwal.a@mamaearth.in <agarwal.a@mamaearth.in>
2021-12-23 22:09:21 +0530

    Firebase and Registration Link to Email

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,7 @@
-import * as firebase from 'firebase';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
+const config = {
     apiKey: "AIzaSyATMgGe-8klQ_SZgy5qTXBm2dnplSGQDw0",
     authDomain: "cyberkart-d7379.firebaseapp.com",
     projectId: "cyberkart-d7379",
@@ -9,8 +10,10 @@ const firebaseConfig = {
     appId: "1:979677956121:web:27dab5dbb1af6ddb484288"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
-export const googleAuthProvider = new firebase.auth.googleAuthProvider();
+// initialize firebase app
+const app = initializeApp(config);
+ 
+// export
+export const auth = getAuth(app);
+ 
+export const googleAuthProvider = new GoogleAuthProvider();
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../../firebase';
+import { sendSignInLinkToEmail } from "firebase/auth";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Register() {
+
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const config = {
+            url: 'http://localhost:3000/register/complete',
+            handleCodeInApp: true
+        }
+
+        await sendSignInLinkToEmail(auth, email, config);
+        toast.success(`Email is sent to ${email}. Click the link to complete your registration.`);
+        window.localStorage.setItem('emailForRegistration', email);
+        setEmail('');
+    }
+
+    const registerForm = () => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="email" 
+                    className="form-control" 
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    autoFocus
+                />
+
+                <button type="submit" className="btn btn-raised">
+                    Register
+                </button>
+
+            </form>
+        )
+    }
+
     return (
-        <div>
-            Register
+        <div className="container p-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h4>Register</h4>
+                    <ToastContainer />
+                    {registerForm()}
+                </div>
+            </div>
         </div>
     )
 }
