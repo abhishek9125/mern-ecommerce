cdf45a5e53b2b8a43176500162914b1052c3313e
agarwal.a@mamaearth.in <agarwal.a@mamaearth.in>
2021-12-23 22:48:30 +0530

    Added: Registration Form

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
+import RegisterComplete from './pages/auth/RegisterComplete';
 import Home from './pages/Home';
 import Header from "./components/Navbar/Header";
 import 'react-toastify/dist/ReactToastify.css';
@@ -16,6 +17,8 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />  
+        <Route exact path="/register/complete" element={<RegisterComplete />} />  
+
       </Routes>
     </BrowserRouter>
   );
