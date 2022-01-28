c77579372f5c9c3c1e45e9ed97b20bdc458416c2
agarwal.a@mamaearth.in <agarwal.a@mamaearth.in>
2022-01-28 19:07:14 +0530

    Added: Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Login from './pages/auth/Login';
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
+import rootReducer from './reducers';
 import 'antd/dist/antd.css';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
