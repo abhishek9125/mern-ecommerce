575e97698183fdd392d68efed6ba06f44d1d783c
agarwal.a@mamaearth.in <agarwal.a@mamaearth.in>
2022-01-29 02:35:49 +0530

    Added: Controllers Architecture

---
+++ server/controllers/auth.js
@@ -0,0 +1,5 @@
+exports.createOrUpdateUser = (req, res) => {
+    res.json({
+        data : 'Hey you hit Node API'
+    })
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/create-or-update-user', (req, res) => {
-    res.json({
-        data : 'Hey you hit Node API'
-    })
-});
+const { createOrUpdateUser } = require('../controllers/auth');
+
+router.get('/create-or-update-user', createOrUpdateUser);
 
 module.exports = router;
