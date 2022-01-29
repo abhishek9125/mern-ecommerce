c7bbb6c00620577db340978f03bc5063e5c69f4f
agarwal.a@mamaearth.in <agarwal.a@mamaearth.in>
2022-01-29 19:33:57 +0530

    Added: Create Category Form

--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AdminNav from '../../../components/Navbar/AdminNav';
+import { toast } from 'react-toastify';
+import { useSelector } from 'react-redux';
+import { createCategory, getCategories, removeCategory } from '../../../functions/category';
 
 function CategoryCreate() {
+
+    const [name, setName] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [categories, setCategories] = useState([]);
+
+    const { user } = useSelector((state) => ({ ...state }));
+
+    useEffect(async () => {
+        const response = await getCategories();
+        setCategories(response.data);
+    },[])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        createCategory({ name }, user.token)
+        .then((response) => {
+            setLoading(false);
+            setName('');
+            toast.success(`${response.data.name} Category Created Successfully..!!`);
+        })
+        .catch((error) => {
+            setLoading(false);
+            if(error.response.status === 400) toast.error(error.response.data)
+            console.log('Error Adding Category : ', error);
+        });
+    }
+
+    const categoryForm = () => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Name</label>
+                    <input 
+                        type="text"
+                        className="form-control"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        autoFocus
+                        required
+                    />
+                    <br />
+                    <button className="btn btn-outline-primary" disabled={loading || name.length < 2}>Save</button>
+                </div>
+            </form>
+        )
+    }
+
     return (
         <div className="container-fluid">
             <div className="row">
-
-            <div className="col-md-2">
-                <AdminNav />
-            </div>
-            <div className="col">
-                Create Category
-            </div>
+                <div className="col-md-2">
+                    <AdminNav />
+                </div>
+                <div className="col">
+                    <h4>Create Category</h4>
+                    {categoryForm()}
+                </div>
             </div>
         </div>
     )
