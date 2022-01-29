b943e8bd724e930238a5ae7ca0a40c92208dd4b9
agarwal.a@mamaearth.in <agarwal.a@mamaearth.in>
2022-01-29 13:04:33 +0530

    Saved: User to Database

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,14 @@
-exports.createOrUpdateUser = (req, res) => {
-    res.json({
-        data : 'Hey you hit Node API'
-    })
+const User = require('../models/user');
+
+exports.createOrUpdateUser = async (req, res) => {
+    
+    const { name, email, picture } = req.user;
+    const user = await User.findOneAndUpdate({ email }, { name, picture }, { new: true });
+    if(user) {
+        res.json(user);
+    } else {
+        const newUser = await new User({ email, name, picture }).save();
+        res.json(newUser);
+    }
+
 }
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,15 @@
 const admin = require('../firebase');
 
-exports.authCheck = (req, res, next) => {
-    console.log('Hello : ', req.headers);
+exports.authCheck = async (req, res, next) => {
+    try {
+        const firebaseUser = await admin.auth().verifyIdToken(req.headers.authtoken);
+        req.user = firebaseUser; // Added to req object to access at controller
+        next();
+    } catch (error) {
+        console.log("Error Validating the Token : ", error);
+        res.status(401).json({
+            error: 'Token is either Invalid or Expired.'
+        })
+    }
     next();
 }
