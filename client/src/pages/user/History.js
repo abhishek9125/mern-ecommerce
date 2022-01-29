33c07365b4e38c06c0d507d633678a0f87c494f0
agarwal.a@mamaearth.in <agarwal.a@mamaearth.in>
2022-01-29 15:25:10 +0530

    Added: User Navbar

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import UserNav from '../../components/Navbar/UserNav';
 
 function History() {
     return (
-        <div>
-            User History
+        <div className="container-fluid">
+            <div className="row">
+
+            <div className="col-md-2">
+                <UserNav />
+            </div>
+            <div className="col">
+                User History
+            </div>
+            </div>
         </div>
     )
 }
