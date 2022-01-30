5ce7d18e25a1d3e217b718456d76409c9d7a3984
abhishek9125 <aagarwal_be17@thapar.edu>
2022-01-30 18:17:55 +0530

    Added: Product Delete Endpoint

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -14,6 +14,16 @@ exports.create = async (req, res) => {
     }
 }
 
+exports.remove = async (req, res) => {
+    try {
+        const deletedProduct = await Product.findOneAndRemove({ slug: req.params.slug }).exec();
+        res.json(deletedProduct);
+    } catch (error) {
+        console.log('Error Creating New Product : ', error);
+        return res.status(400).send('Error Deleting the Product');
+    }
+}
+
 exports.listAll = async (req, res) => {
     try {
         let products = await Product.find({})
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, listAll } = require('../controllers/product');
+const { create, listAll, remove } = require('../controllers/product');
 
 const { authCheck, adminCheck } = require('../middlewares/auth');
 
 router.post('/product', authCheck, adminCheck, create);
 router.get('/products/:count', listAll);
+router.delete('/product/:slug', authCheck, adminCheck, remove);
 
 module.exports = router;
