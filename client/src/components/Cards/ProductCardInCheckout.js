fa83a9e1426c43a0d666cf700a05fa1c8f336125
abhishek9125 <aagarwal_be17@thapar.edu>
2022-02-03 23:40:04 +0530

    Added: React Image Modal

--- client/src/components/Cards/ProductCardInCheckout.js
+++ client/src/components/Cards/ProductCardInCheckout.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import ModalImage from 'react-modal-image';
+import laptop from '../../images/laptop.png';
 
 function ProductCardInCheckout({ product }) {
     return (
         <tbody>
             <tr>
-                <td>Image</td>
+                <td>
+                    <div style={{ width: '100px', height: 'auto' }}>
+                        {
+                            product.images.length ? 
+                            <ModalImage 
+                                small={product.images[0].url}
+                                large={product.images[0].url}
+                            /> :
+                            <ModalImage 
+                                small={laptop}
+                                large={laptop}
+                            />
+                        }
+                    </div>
+                </td>
                 <td>{product.title}</td>
                 <td>${product.price}</td>
                 <td>{product.brand}</td>
