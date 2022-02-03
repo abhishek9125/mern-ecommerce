0fde1a98a48ece7803265a46fab8cf7b20ae8207
abhishek9125 <aagarwal_be17@thapar.edu>
2022-02-03 23:21:00 +0530

    Added: Cart Page Summary

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -5,16 +5,55 @@ import { Link } from 'react-router-dom';
 
 function Cart() {
 
-
     const dispatch = useDispatch();
     const { user, cart } = useSelector((state) => ({ ...state }));
 
+    const getTotal = () => {
+        return cart.reduce((currentValue, nextValue) => {
+            return currentValue + (nextValue.price * nextValue.count)
+        },0);
+    }
 
     return (
-        <div className="container-fluid">
+        <div className="container-fluid pt-2">
+
             <div className="row">
-                <h4>Cart</h4>
-                
+                <div className="col-md-8">
+                    <h4>Cart / {cart.length} Products</h4>
+                    {
+                        !cart.length ? 
+                        <p>No Products in Cart. <Link to='/shop'>Continue Shopping</Link></p> : 
+                        <div>Show Cart Items</div>
+                    }
+                </div>
+                <div className="col-md-4">
+                    <h4>Order Summary</h4>
+                    <hr />
+                    <p>Products</p>
+                    {
+                        cart.map((c,i) => {
+                            return (
+                                <div key={i}>
+                                    <p>
+                                        {c.title} x {c.count} = ${c.price * c.count}
+                                    </p>
+                                </div>
+                            )
+                        })
+                    }
+                    <hr />
+                    Total: <b>${getTotal()}</b>
+                    <hr />
+                    {
+                        user ? 
+                        <button className="btn btn-sn btn-primary btn-raised">
+                            Proceed To Checkout
+                        </button> :
+                        <button className="btn btn-sn btn-primary btn-raised">
+                            Login To Checkout
+                        </button>
+                    }
+                </div>
             </div>
         </div>
     )
