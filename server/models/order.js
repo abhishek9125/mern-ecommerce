bd4f700ecdd1651bf687028eefed733e8961edfc
abhishek9125 <aagarwal_be17@thapar.edu>
2022-02-19 23:52:32 +0530

    Added: Order Schema

---
+++ server/models/order.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const orderSchema = new mongoose.Schema({
+    products: [
+        {
+            product: {
+                type: ObjectId,
+                ref: 'Product'
+            },
+            count: Number,
+            color: String
+        },
+    ],
+    paymentIntent: {},
+    orderStatus: {
+        type: String,
+        default: 'Not Processed',
+        enum: [
+            'Not Processed', 'Processing', 'Dispatched', 'Cancelled', 'Delivered'
+        ]
+    },
+    orderedBy: {
+        type: ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Order', orderSchema); 
