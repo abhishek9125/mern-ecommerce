b19793b944cb75f987bdf89cd93aa6b114ea5f63
abhishek9125 <aagarwal_be17@thapar.edu>
2022-02-20 16:47:49 +0530

    Added: User Order Info

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -39,11 +39,38 @@ function History() {
         })
     }
 
-    const showOrderInTable = () => {
+    const showOrderInTable = (order) => {
         return (
-            <p>
-                Each Order
-            </p>
+            <table className="table table-bordered">
+                <thead className="thead-light">
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Brand</th>
+                        <th scope="col">Color</th>
+                        <th scope="col">Count</th>
+                        <th scope="col">Shipping</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        order.products.map((p,i) => {
+                            return (
+                                <tr key={i}>
+                                    <td>
+                                        <b>{p.product.title}</b>
+                                    </td>
+                                    <td>{p.product.price}</td>
+                                    <td>{p.product.brand}</td>
+                                    <td>{p.color}</td>
+                                    <td>{p.count}</td>
+                                    <td>{p.product.shipping == 'Yes' ? <CheckCircleOutlined style={{ color: 'green' }} /> : <CloseCircleOutlined style={{ color: 'red' }} />}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
         )
     }
 
